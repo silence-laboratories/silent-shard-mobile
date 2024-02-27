6c08959b923ce52beab0475deebf3811410cc010
Daksh Garg <daksh@silencelaboratories.com>
2024-02-28 00:00:35 +0530

    Error handling for repairing

--- lib/screens/scanner_screen.dart
+++ lib/screens/scanner_screen.dart
@@ -182,7 +182,7 @@ class _ScannerScreenState extends State<ScannerScreen> {
   void _cancelPairing(bool showTryAgain, [dynamic error]) {
     _pairingOperation?.cancel();
     _updatePairingState(ScannerScreenPairingState.ready);
-    if (error.toString().contains('INVALID_BACKUP_DATA')) {
+    if (error.toString().contains('NO_BACKUP_DATA_WHILE_REPAIRING')) {
       Navigator.of(context).push(
         MaterialPageRoute(
           builder: (context) => NoBackupFoundWhileRepairingScreen(onPress: () {
