ffb4ca4ee598a94611db23a2e26e27a61f06e27b
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-04-15 10:37:45 +0700

    refactor(bullet_list): Create bullet list widget to avoid duplicate code

--- lib/screens/scanner/guide_me_tabs.dart
+++ lib/screens/scanner/guide_me_tabs.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:silentshard/constants.dart';
-import 'package:silentshard/screens/components/bullet.dart';
 import 'package:silentshard/screens/components/button.dart';
+import 'package:silentshard/screens/scanner/bullet_list.dart';
 import 'package:silentshard/screens/scanner/support_wallets_list.dart';
 import 'package:silentshard/types/support_wallet.dart';
 
@@ -84,55 +84,7 @@ class GuideMeTabControllerState extends State<GuideMeTabController> {
                                   ],
                                 ),
                                 const Gap(defaultSpacing * 2),
-                                Bullet(
-                                  child: RichText(
-                                    text: TextSpan(
-                                      children: <TextSpan>[
-                                        TextSpan(text: 'Open ', style: textTheme.displaySmall),
-                                        TextSpan(
-                                            text: 'snap.silencelaboratories.com',
-                                            style: textTheme.displaySmall?.copyWith(fontWeight: FontWeight.bold)),
-                                        TextSpan(text: ' in your desktop.', style: textTheme.displaySmall),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Bullet(
-                                  child: Text("Connect Silent Shard Snap with your MetaMask extension.", style: textTheme.displaySmall),
-                                ),
-                                if (widget.isRePairing == true)
-                                  Bullet(
-                                    child: RichText(
-                                      text: TextSpan(
-                                        children: <InlineSpan>[
-                                          TextSpan(text: 'If account is already present:  press on the', style: textTheme.displaySmall),
-                                          const WidgetSpan(
-                                            child: Icon(
-                                              Icons.more_vert,
-                                              size: 20,
-                                            ),
-                                          ),
-                                          TextSpan(
-                                              text: 'icon and click on ‘Recover account on phone’ and follow the instructions',
-                                              style: textTheme.displaySmall),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                Bullet(
-                                  child: RichText(
-                                    text: TextSpan(
-                                      children: <InlineSpan>[
-                                        TextSpan(text: 'Scan QR code with ', style: textTheme.displaySmall),
-                                        WidgetSpan(
-                                          child: Image.asset('assets/icon/silentShardLogo.png', height: 20, width: 20),
-                                        ),
-                                        TextSpan(text: ' Silent Shard', style: textTheme.displaySmall?.copyWith(fontWeight: FontWeight.bold)),
-                                        TextSpan(text: ' logo and connect this device.', style: textTheme.displaySmall),
-                                      ],
-                                    ),
-                                  ),
-                                ),
+                                BulletList(isOtherWalletInstructor: false, isRePairing: widget.isRePairing)
                               ]),
                             ),
                           ),
@@ -150,28 +102,7 @@ class GuideMeTabControllerState extends State<GuideMeTabController> {
                                   },
                                 ),
                                 const Gap(defaultSpacing * 2),
-                                Bullet(
-                                  child: Text(
-                                      "Head over to your favourite SilentShard-supported wallets (coming soon...) on your browser to desktop.",
-                                      style: textTheme.displaySmall),
-                                ),
-                                Bullet(
-                                  child: Text("Follow the instructions to create a new distributed wallet.", style: textTheme.displaySmall),
-                                ),
-                                Bullet(
-                                  child: RichText(
-                                    text: TextSpan(
-                                      children: <InlineSpan>[
-                                        TextSpan(text: 'Scan QR code with ', style: textTheme.displaySmall),
-                                        WidgetSpan(
-                                          child: Image.asset('assets/icon/silentShardLogo.png', height: 20, width: 20),
-                                        ),
-                                        TextSpan(text: ' Silent Shard', style: textTheme.displaySmall?.copyWith(fontWeight: FontWeight.bold)),
-                                        TextSpan(text: ' logo and connect this device.', style: textTheme.displaySmall),
-                                      ],
-                                    ),
-                                  ),
-                                ),
+                                const BulletList(isOtherWalletInstructor: true)
                               ]),
                             ),
                           ),
