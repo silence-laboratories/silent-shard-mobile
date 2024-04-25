0a67ac432eadb33ece2161c8dbbd4f8b7f01f9f2
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-04-25 14:18:21 +0700

    feat(wallet_card): Display wallet info based on their id

--- lib/types/support_wallet.dart
+++ lib/types/support_wallet.dart
@@ -4,7 +4,7 @@ class SupportWallet {
 
   SupportWallet({required this.name, required this.icon});
 
-  factory SupportWallet.fromJson(Map<String, dynamic> json) {
+  factory SupportWallet.fromJson(Map<String, String> json) {
     return SupportWallet(
       name: json['name'] ?? (throw ArgumentError('name is null')),
       icon: json['icon'] ?? (throw ArgumentError('icon is null')),
