9f571105294060cf5c9d5b1250de7ae9059c0dc2
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-05-05 00:37:59 +0700

    App show already present wallet while pairing the same wallet again

--- lib/screens/wallet/wallet_screen.dart
+++ lib/screens/wallet/wallet_screen.dart
@@ -184,7 +184,7 @@ class _WalletScreenState extends State<WalletScreen> with WidgetsBindingObserver
               onPressed: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
-                    builder: (context) => PairScreen(fromWalletScreen: true),
+                    builder: (context) => const PairScreen(),
                   ),
                 );
               },
