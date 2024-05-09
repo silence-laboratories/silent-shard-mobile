6d09df98f4a191757fc1be0e4bc860396f1d54a3
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-05-09 17:46:18 +0700

    Refactor multi wallet mismatch screen

--- lib/screens/error/multi_wallet_mismatch_screen.dart
+++ lib/screens/error/multi_wallet_mismatch_screen.dart
@@ -1,8 +1,6 @@
 // Copyright (c) Silence Laboratories Pte. Ltd.
 // This software is licensed under the Silence Laboratories License Agreement.
 
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:silentshard/constants.dart';
@@ -100,7 +98,7 @@ class MultiWalletMismatchScreen extends StatelessWidget {
                                     height: 36,
                                   )),
                                   Container(
-                                      margin: EdgeInsets.symmetric(horizontal: defaultSpacing * 4),
+                                      margin: const EdgeInsets.symmetric(horizontal: defaultSpacing * 4),
                                       child: const Icon(
                                         Icons.arrow_forward,
                                         color: Color(0xFFF6F7F9),
@@ -116,51 +114,14 @@ class MultiWalletMismatchScreen extends StatelessWidget {
                           ),
                         ),
                       ),
-                      // Container(
-                      //   decoration: BoxDecoration(
-                      //     border: Border.all(
-                      //       color: Color(0xFF991B1B),
-                      //     ),
-                      //     borderRadius: BorderRadius.circular(10),
-                      //     color: Color(0xFFF87171).withOpacity(0.1),
-                      //   ),
-                      //   padding: EdgeInsets.all(defaultSpacing * 1.5),
-                      //   child: Row(
-                      //     crossAxisAlignment: CrossAxisAlignment.start,
-                      //     children: [
-                      //       Image.asset("assets/images/information-circle_light.png", height: 18, color: Color(0xFFF87171)),
-                      //       Gap(defaultSpacing),
-                      //       Flexible(
-                      //         child: Text(
-                      //           'Restoring the selected account will replace the existing one on your browser',
-                      //           style: textTheme.bodySmall?.copyWith(
-                      //             color: Color(0xFFFECACA),
-                      //           ),
-                      //         ),
-                      //       ),
-                      //     ],
-                      //   ),
-                      // ),
-                      // Gap(defaultSpacing * 2),
-                      // Button(
-                      //   onPressed: () {
-                      //     Navigator.of(context).pop();
-                      //     onBack();
-                      //   },
-                      //   type: ButtonType.secondary,
-                      //   child: Text(
-                      //     'Go back',
-                      //     style: textTheme.displaySmall?.copyWith(color: primaryColor, height: 2),
-                      //   ),
-                      // ),
-                      Gap(defaultSpacing * 2),
+                      const Gap(defaultSpacing * 2),
                       Button(
                         onPressed: () {
                           Navigator.of(context).pop();
                           onContinue();
                         },
                         child: Text(
-                          'Back to',
+                          'Back to Pairing',
                           style: textTheme.displaySmall?.copyWith(height: 2),
                         ),
                       ),
