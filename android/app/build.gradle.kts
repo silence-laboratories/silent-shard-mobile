e7df2dc5e55c2b66ac7de32c457e7d3211d01fb1
dakshsilence <daksh@silencelaboratories.com>
2026-05-06 12:55:19 +0530

    Small fix for prod release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         applicationId = "com.silencelaboratories.silentshard"
-        minSdk = 24
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
